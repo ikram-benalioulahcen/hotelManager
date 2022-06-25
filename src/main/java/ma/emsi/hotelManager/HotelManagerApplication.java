e5cb4a607e60edf8faebf0b8ca81ca85acad1f4b
BENALI OULAHCEN Ikram <105321428+Karanosi@users.noreply.github.com>
2022-06-25 21:32:56 +0100

    Initial commit

---
+++ src/main/java/ma/emsi/hotelManager/HotelManagerApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.hotelManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/emsi/hotelManager/HotelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.hotelManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
