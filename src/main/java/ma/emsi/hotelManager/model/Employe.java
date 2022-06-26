33178cf22131ca1413e39e369e8f37f55c0b0bb6
BENALI OULAHCEN Ikram <105321428+Karanosi@users.noreply.github.com>
2022-06-26 10:51:48 +0100

    employe modifs

--- src/main/java/ma/emsi/hotelManager/model/Employe.java
+++ src/main/java/ma/emsi/hotelManager/model/Employe.java
@@ -1,16 +1,15 @@
 package ma.emsi.hotelManager.model;
 
-import jdk.jfr.DataAmount;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import net.bytebuddy.dynamic.loading.InjectionClassLoader;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import static javax.persistence.GenerationType.AUTO;
+
+import java.time.LocalDate;
 
 @Data
 @Entity
@@ -22,5 +21,8 @@ public class Employe {
     private Long id;
     private String nom;
     private String prenom;
+    private String cin;
+    private LocalDate dateNaissance;
+    
 
 }
