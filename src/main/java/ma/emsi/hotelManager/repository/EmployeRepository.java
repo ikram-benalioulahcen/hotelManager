ff8d29f29c361ad3decfe389f2112c4b9a54df0e
BENALI OULAHCEN Ikram <105321428+Karanosi@users.noreply.github.com>
2022-06-29 00:29:52 +0100

    Update EmployeRepository.java

--- src/main/java/ma/emsi/hotelManager/repository/EmployeRepository.java
+++ src/main/java/ma/emsi/hotelManager/repository/EmployeRepository.java
@@ -6,12 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EmployeRepository extends JpaRepository<Employe, Long> {
-    Employe findByName();
-
-
-    public Employe findById(int id);
-
-    public List<Employe> findAll();
-
-
+    //Employe findByName();
 }
